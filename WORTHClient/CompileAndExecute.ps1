af5903cb10894feba33644dcd81e0c1eb1bed71d
Alessandro Di Biase <a.dibiase5@studenti.unipi.it>
2021-01-15 12:13:43 +0100

    Versione finale, manca da sistemare la chat e se ho tempo i warnings

--- WORTHClient/CompileAndExecute.ps1
+++ WORTHClient/CompileAndExecute.ps1
@@ -1,4 +1,4 @@
 javac -d ./bin -cp ".;./lib/gson-2.8.6.jar;" ./src/*.java
 cd bin
-java -cp ".;./lib/gson-2.8.6.jar;" Client
+java -cp ".;../lib/gson-2.8.6.jar;" Client
 
--- WORTHServer/CompileAndExecute.ps1
+++ WORTHServer/CompileAndExecute.ps1
@@ -1,4 +1,4 @@
 javac -d ./bin -cp ".;./lib/gson-2.8.6.jar;./lib/Serializers.jar;" ./src/*.java
 cd bin
-java -cp ".;./lib/gson-2.8.6.jar;./lib/Serializers.jar;" ServerMain
+java -cp ".;../lib/gson-2.8.6.jar;../lib/Serializers.jar;" ServerMain
 
